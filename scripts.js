008a144ffca5da1a6293b3f3764b050ff3fba231
ye14ev <32113616+ye14ev@users.noreply.github.com>
2017-11-23 19:13:32 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
-function pushMe(){
-	document.getElementById("pushMe").innerHTML= "Hurrah!";
+var num="";
+
+function getNum(){
+
+var getNum = document.getElementById ('input').value;
+for(i=0;i<=getNum;i++){
+console.log (i);
+document. getElementById('output').innerHTML += i+"\n";
+}
 }
